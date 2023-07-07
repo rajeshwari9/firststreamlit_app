ccc72394cba5984955fd3690b503507d1dddef8b
Rajeshwari Guddeti <rajeshwarireddyg@gmail.com>
2023-07-06 18:17:24 -0600

    Update streamlit_app.py
    Fruityvice committed

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,5 +32,3 @@ streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
-
-
